c810d83cf35284c7a9dfb2c1c2cbdc7cc50305c5
swampbear <jobjorn.myren@gmail.com>
2026-02-09 20:41:45 +0100

    feat: add exit and help commands

--- repl.go
+++ repl.go
@@ -1,8 +1,32 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startREPL() {
+	//bufio scanner for reading input
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		words := cleanInput(scanner.Text())
+		cmd_txt := words[0]
+		cmd, exists := getCommands()[cmd_txt]
+		if exists {
+			cmd.callback()
+		} else {
+			fmt.Println("unknown command")
+		}
+
+	}
+
+}
 
 func cleanInput(text string) []string {
 	new_text := strings.ToLower(text)
